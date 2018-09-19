c9ad4f49457e5b68afe844de2e123f75578a66e3
Micah Caffey <mrcaffey@gmail.com>
2018-09-19 08:25:33 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React-Rails-Restaurant</h1>
+          <h3>This page is currently under construction. Check back soon!</h3>
+        </header>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
